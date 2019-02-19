9096b7065b7679c30ea2264397476d0f2414fbb0
Ryan Sise <kerneltux0@gmail.com>
2019-02-19 17:36:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,6 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
   monopoly
-		
 end
 
 def monopoly_with_fourth_tier
