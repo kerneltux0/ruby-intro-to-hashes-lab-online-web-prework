cf64aa508fef5f14e996557269c627b084904159
Ryan Sise <kerneltux0@gmail.com>
2019-02-19 17:29:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,6 +15,7 @@ def monopoly_with_second_tier
   monopoly = {}
   monopoly = {:railroads => {}}
   monopoly[:railroads][:pieces] = 4
+  monopoly
   monopoly[:railroads] = {:names => {}}
   monopoly
 end
